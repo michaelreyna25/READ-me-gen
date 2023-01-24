655d32ef8def7a66e9829a2db5bdc9edbae5dbb6
Michael Reyna <25.27.reyna@gmail.com>
2023-01-23 18:52:04 -0700

    added the generated readme with git ignore

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+// Ask the title of the readme, description, installation instructions, usage information, contribution guidelines, and test instructions
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
