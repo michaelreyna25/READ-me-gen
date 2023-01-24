0bdc605447a2d1ba5748d7f34996be526ac3e6db
Michael Reyna <25.27.reyna@gmail.com>
2023-01-23 22:21:18 -0700

    created array of questions

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
+const package = require ('package.json');
+const inquirer = require('inquirer');
+const fs = require ('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    }, 
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description',
+    }, 
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation',
+    }, 
+    {
+        type: 'input',
+        message: 'How would I use this application?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Who contributed to this project?',
+        name: 'contributing',
+    }, 
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'tests',
+    }, 
+    {
+        type: 'list',
+        message: 'What license would you like to use?',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0',],
+        default: 'MIT',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    }
+    ];
 // Ask the title of the readme, description, installation instructions, usage information, contribution guidelines, and test instructions
 
 // TODO: Create a function to write README file
